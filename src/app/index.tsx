f7da1e64575772f87ec2fc4bd5a7c9068c4dcb37
André Angeloni <andrefangeloni@gmail.com>
2024-02-10 00:03:23 -0300

    feat: category flatlist

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,34 @@
-import { View } from 'react-native'
+import { FlatList, View } from 'react-native'
 
-import { Header } from '@/components'
+import { Category, Header } from '@/components'
 
-const Home = () => (
-  <View className="flex-1 pt-8">
-    <Header title="Faça seu pedido" cartQuantity={1} />
-  </View>
-)
+import { CATEGORIES } from '@/utils/data/products'
+import { useState } from 'react'
+
+const Home = () => {
+  const [category, setCategory] = useState(CATEGORIES[0])
+
+  return (
+    <View className="flex-1 pt-8">
+      <Header title="Faça seu pedido" cartQuantity={1} />
+
+      <FlatList
+        horizontal
+        data={CATEGORIES}
+        className="max-h-10 mt-5"
+        keyExtractor={(item) => item}
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => (
+          <Category
+            title={item}
+            isSelected={item === category}
+            onPress={() => setCategory(item)}
+          />
+        )}
+        contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+      />
+    </View>
+  )
+}
 
 export default Home
---
+++ src/components/category.tsx
@@ -0,0 +1,19 @@
+import { clsx } from 'clsx'
+import { Text, Pressable, PressableProps } from 'react-native'
+
+type Props = PressableProps & {
+  title: string
+  isSelected?: boolean
+}
+
+export const Category = ({ title, isSelected, ...rest }: Props) => (
+  <Pressable
+    className={clsx(
+      'bg-slate-800 px-4 justify-center rounded-md h-10',
+      isSelected && 'border-2 border-lime-300',
+    )}
+    {...rest}
+  >
+    <Text className="text-slate-100 font-subtitle text-sm">{title}</Text>
+  </Pressable>
+)
